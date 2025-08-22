cf2d159ade65b88666ef91d079df96ead37ae9ab
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-22 08:14:35 -0400

    Change input to textarea in spec

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe "posts/new", type: :feature do
     it "has error class on bad fields" do
       expect(page).to have_css(".field_with_errors input[name=title]")
       expect(page).to have_css(".field_with_errors input[name=category]")
-      expect(page).to have_css(".field_with_errors input[name=content]")
+      expect(page).to have_css(".field_with_errors textarea[name=content]")
     end
   end
 end
